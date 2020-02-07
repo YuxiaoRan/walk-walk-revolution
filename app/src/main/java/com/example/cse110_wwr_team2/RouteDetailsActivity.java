577b36d2abd970edc2491f42c3165100399acc4f
Mingjie Zhu <36128312+MingjieZhuEmily@users.noreply.github.com>
2020-02-06 17:41:09 -0800

    UI for Route Activity, adding a new route, and route details

---
+++ app/src/main/java/com/example/cse110_wwr_team2/RouteDetailsActivity.java
@@ -0,0 +1,46 @@
+package com.example.cse110_wwr_team2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class RouteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_details);
+
+        // get the string passed from route activity
+        Intent intent = getIntent();
+        String route = intent.getExtras().getString("route");
+        String[] info = route.split(" ");
+
+        // set the text in UI
+        TextView RouteName = findViewById(R.id.route_name);
+        RouteName.setText(info[0]);
+        TextView StartPoint = findViewById(R.id.start_point);
+        StartPoint.setText(info[1]);
+
+        FloatingActionButton fab = findViewById(R.id.done_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRoute();
+            }
+        });
+
+    }
+
+    public void launchRoute(){
+        Intent intent = new Intent(this, RouteActivity.class);
+        startActivity(intent);
+    }
+
+
+}
