43d01afcc858d075cfd967381127d5e38506cd94
Mingjie Zhu <36128312+MingjieZhuEmily@users.noreply.github.com>
2020-02-12 19:02:41 -0800

    Complete Route Screen
    Change of logic of passing object between activities, pass object instead of array of String

--- app/src/main/java/com/example/cse110_wwr_team2/Route.java
+++ app/src/main/java/com/example/cse110_wwr_team2/Route.java
@@ -1,15 +1,17 @@
 package com.example.cse110_wwr_team2;
 
+import java.io.Serializable;
+
 /*
  * This is the simplest class for a route
  *      String start_point: the string that stores the start point of the route
  *      String name: the string that saves the name of the route
  *      int step_cnt: the step count of this route
  */
-public class Route {
+public class Route implements Serializable {
     private String start_point;
     private String name;
-    private int step_cnt;
+    private long step_cnt;
 
     public Route(String start_point, String name, int step_cnt){
         this.start_point = start_point;
@@ -25,19 +27,19 @@ public class Route {
         return this.name;
     }
 
-    public int getStepCnt(){
+    public long getStepCnt(){
         return this.step_cnt;
     }
 
-    public void updateStep(int new_step_cnt){
+    public void updateStep(long new_step_cnt){
         this.step_cnt = new_step_cnt;
     }
 
     public String toString(){
-        return "Route Name: " + this.name + " Start point: " + this.start_point;
+        return "Route Name: " + this.name + " Start point: " + this.start_point + " Step Count: " + this.step_cnt;
     }
 
     public String[] toList(){
-        return new String[]{this.name, this.start_point, Integer.toString(this.step_cnt)};
+        return new String[]{this.name, this.start_point, Long.toString(this.step_cnt)};
     }
 }
