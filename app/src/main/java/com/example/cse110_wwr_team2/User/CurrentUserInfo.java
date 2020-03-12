fd5473b6978d2f296626938c12d49f5eadf6bd7e
Zheng Zeng <leozz1999@163.com>
2020-03-12 01:47:24 -0700

    Adding Mockito Tester

--- app/src/main/java/com/example/cse110_wwr_team2/User/CurrentUserInfo.java
+++ app/src/main/java/com/example/cse110_wwr_team2/User/CurrentUserInfo.java
@@ -8,13 +8,11 @@ public class CurrentUserInfo {
     public static String getId(Context context){
         SharedPreferences sharedPreferences = context.getSharedPreferences("user",Context.MODE_PRIVATE);
         return sharedPreferences.getString("id", null);
-//        return "8B7qKDUGQubzG0lajb16F5gML233";
     }
 
     public static String getTeamId(Context context){
         SharedPreferences sharedPreferences = context.getSharedPreferences("user", Context.MODE_PRIVATE);
         return sharedPreferences.getString("teamID", null);
-//        return "1";
     }
 
 }
